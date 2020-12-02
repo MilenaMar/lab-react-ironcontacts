6831006d0f6e407c4480ff28201728a078f5a927
Milena <milykar13@icloud.com>
2020-12-02 17:34:10 +0000

    Iteration 1 to 3 completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import contacts from './contacts.json';
 import './App.css';
 
-function App() {
+export default class App extends Component  {
+  state = {
+    contactshow: contacts.slice(0, 5)
+  }
+
+  getRandom = () => {
+    let randomCo = contacts[Math.floor(Math.random() * contacts.length)]
+  if (this.state.contactshow.includes(randomCo)){
+    this.setState({contactShow:this.state.contactshow})
+  } else {
+    this.setState({contactShow:this.state.contactshow.push(randomCo)})
+  }
+  }
+
+byName = ()=> {
+this.setState({contactShow:this.state.contactshow.sort
+  ((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
+})
+}
+
+byPopularity = ()=> {
+  this.setState({contactShow:this.state.contactshow.sort
+    ((a,b) => (a.popularity > b.popularity) ? 1 : ((b.popularity > a.popularity) ? -1 : 0))})
+}
+
+ render () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <div className="buttons">
+      <button onClick={this.getRandom}>Add Random Contact</button>
+      <button onClick={this.byName}>Sort by Name</button>
+      <button onClick={this.byPopularity}>Sort by Popularity</button>
+      </div>
+      <div className="description">
+        <p>Picture</p>
+        <p>Name</p>
+        <p>Popularity</p>
+      </div>
+      {this.state.contactshow.map(e => 
+      <div key={e.name} className="celebrity">
+     <img src={e.pictureUrl} alt="celebrity"></img>
+      <p>{e.name}</p>
+      <p>{(e.popularity).toFixed(2)}</p>
+      </div>)}  
     </div>
   );
 }
+}
 
-export default App;
+
